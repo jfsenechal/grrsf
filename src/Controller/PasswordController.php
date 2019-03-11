bf68876911cadda907218eb45fd9798ebd69beb0
jfsenechal <jfsenechal@gmail.com>
2019-03-11 21:16:26 +0100

    User

---
+++ src/Controller/PasswordController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserPasswordType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/grr/password")
+ */
+class PasswordController extends AbstractController
+{
+    /**
+     * @Route("/{id}", name="grr_user_password")
+     */
+    public function edit(Request $request, User $user)
+    {
+        $form = $this->createForm(UserPasswordType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+        }
+
+        return $this->render(
+            'password/edit.html.twig',
+            [
+                'user' => $user,
+                'form' => $form->createView(),
+            ]
+        );
+    }
+}
