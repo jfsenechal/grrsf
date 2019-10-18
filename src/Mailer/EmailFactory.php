6bb4c4aaa4f5b19937704c2de666d5eb2d653ed7
jfsenechal <jfsenechal@gmail.com>
2019-10-18 15:21:34 +0200

    mailer

---
+++ src/Mailer/EmailFactory.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of GrrSf application
+ * @author jfsenechal <jfsenechal@gmail.com>
+ * @date 18/10/19
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Mailer;
+
+
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mime\Email;
+
+class EmailFactory
+{
+    private function __construct()
+    {
+    }
+
+    public static function createNew()
+    {
+        return new Email();
+    }
+
+    public static function createNewTemplated()
+    {
+        return new TemplatedEmail();
+    }
+}
---
+++ src/Mailer/GrrMailer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file is part of GrrSf application
+ * @author jfsenechal <jfsenechal@gmail.com>
+ * @date 18/10/19
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Mailer;
+
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mime\Email;
+
+class GrrMailer
+{
+    /**
+     * @var Mailer
+     */
+    private $mailer;
+
+    public function __construct(Mailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    public function send(Email $email)
+    {
+        try {
+            $this->mailer->send($email);
+        } catch (TransportExceptionInterface $e) {
+        }
+    }
+}
