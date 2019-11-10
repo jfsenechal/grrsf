8491d0613b08c09d98fd6bec5b270cb4e3ceb4b9
jfsenechal <jfsenechal@gmail.com>
2019-11-10 22:13:57 +0100

    add swift mailer

--- config/bundles.php
+++ config/bundles.php
@@ -23,4 +23,5 @@ return [
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
