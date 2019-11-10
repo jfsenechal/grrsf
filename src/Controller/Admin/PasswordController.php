c3848ca9170f93a890b877aff01b2f14ba735ec2
jfsenechal <jfsenechal@gmail.com>
2019-11-10 22:05:00 +0100

    fix return type

--- src/Controller/Admin/PasswordController.php
+++ src/Controller/Admin/PasswordController.php
@@ -10,7 +10,9 @@ use App\Security\PasswordHelper;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -46,7 +48,7 @@ class PasswordController extends AbstractController
     /**
      * @Route("/{id}", name="grr_admin_user_password")
      */
-    public function edit(Request $request, UserInterface $user): \Symfony\Component\HttpFoundation\RedirectResponse
+    public function edit(Request $request, UserInterface $user): Response
     {
         $form = $this->createForm(UserPasswordType::class, $user);
         $form->handleRequest($request);
